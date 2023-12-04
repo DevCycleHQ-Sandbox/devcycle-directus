a78daaf7d15e19345f93891a318808d7f3e93162
Andrew MacLean <andrewdmaclean@gmail.com>
2023-12-04 12:50:45 -0800

    Update Netlify.toml

--- src/_data/directus.js
+++ src/_data/directus.js
@@ -5,9 +5,17 @@ const directusApiUrl = process.env.DIRECTUS_API_URL;
 module.exports = async function() {
   let url = directusApiUrl;
 
-  /* This returns a promise */
-  return EleventyFetch(url, {
-    duration: "60s", // save for 60 seconds
-    type: "json"    // we’ll parse JSON for you
-  });
+  try {
+    // Attempt to fetch data
+    let response = await EleventyFetch(url, {
+      duration: "60s", // save for 60 seconds
+      type: "json"    // parse JSON
+    });
+    return response;
+  } catch (error) {
+    // Handle errors gracefully
+    console.error("Fetch error:", error.message);
+    // Return an empty object or some default data
+    return {}; // or return some default/fallback data
+  }
 };
