9d4986326b22b9569dad4d0f16bbdc9318ec1c58
Andrew MacLean <andrewdmaclean@gmail.com>
2024-01-17 17:21:00 -0800

    Update toml

--- src/devcycle.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  const signupSuccess = urlParams.get("signupSuccess");
-  if (signupSuccess) {
-    document.getElementById("signup-message").style.display = "block";
-  }
-});
